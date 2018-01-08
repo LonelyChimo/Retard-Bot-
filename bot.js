32dea4280ae609df7d46fabfe236250fa995d547
LonelyChimo <35236456+LonelyChimo@users.noreply.github.com>
2018-01-08 22:14:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,24 +7,24 @@ client.on('ready', () => {
 });
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if (!message.content.startsWith(!) || message.author.bot) return;
 
-  if (message.content.startsWith(config.prefix + "ping")) {
+  if (message.content.startsWith(! + "ping")) {
     message.channel.send("pong!");
   } else
-  if (message.content.startsWith(config.prefix + "foo")) {
+  if (message.content.startsWith(! + "foo")) {
     message.channel.send("bar!");
   } else
-  if (message.content.startsWith(config.prefix + "colorized")) {
+  if (message.content.startsWith(! + "colorized")) {
     message.channel.send("https://goo.gl/z39Eh9");
   } else
-  if (message.content.startsWith(config.prefix + "rekt")) {
+  if (message.content.startsWith(! + "rekt")) {
     message.channel.send("YOU JUST GOT REKT SON!!!");
   } else
-  if (message.content.startsWith(config.prefix + "info")) {
+  if (message.content.startsWith(! + "info")) {
     message.channel.send("This Bot is nowhere near done. Chimo is constantly adding stuff for you to enjoy.Leave 10000 Likes. Press F for Respect");
   } else
-  if (message.content.startsWith(config.prefix + "help")) {
+  if (message.content.startsWith(! + "help")) {
     message.channel.send("this is a list of commands http://collabedit.com/5qxb7");
   } 
 });
