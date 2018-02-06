ca33cf7731396e3be57dff0057b4e90a102edf54
LonelyChimo <35236456+LonelyChimo@users.noreply.github.com>
2018-02-06 15:18:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,8 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame('Spaghet')
 });
 
 client.on("message", (message) => {
@@ -17,7 +18,7 @@ client.on("message", (message) => {
     message.channel.send("bar!");
   } else
   if (message.content.startsWith(config.prefix + "colorized")) {
-    message.channel.send("https://goo.gl/z39Eh9%22);
+    message.channel.send("https://goo.gl/z39Eh9");
   } else
   if (message.content.startsWith(config.prefix + "rekt")) {
     message.channel.send("YOU JUST GOT REKT SON!!!");
@@ -26,7 +27,7 @@ client.on("message", (message) => {
     message.channel.send("This Bot is nowhere near done. Chimo is constantly adding stuff for you to enjoy.Leave 10000 Likes. Press F for Respect");
   } else
   if (message.content.startsWith(config.prefix + "help")) {
-    message.channel.send("this is a list of commands http://collabedit.com/5qxb7%22);
+    message.channel.send("this is a list of commands http://collabedit.com/5qxb7");
   } 
 });
 
