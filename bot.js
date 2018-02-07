0353545bac57ce8e797bd0fb63c6118d70295b7c
LonelyChimo <35236456+LonelyChimo@users.noreply.github.com>
2018-02-07 11:11:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ const config = require("./config.json");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame('with Chatters dirty mum')
 });
 
 client.on("message", (message) => {
